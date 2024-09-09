8a508fe9f8cc3f55a174f9ecdab9bf7b594a95ea
SudhanshuHiringtech <sudhanshu@hiringtech.in>
2024-09-09 13:09:38 +0530

    Update User.js

--- model/User.js
+++ model/User.js
@@ -2,7 +2,9 @@
 
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../serviceAccount.json");
+
+const serviceAccount = require("../serviceAccount.json") || JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
