cf4e46b977ada38b9c1d4757e166dda6af05ee0a
sudhanshu30602 <sudhanshugupta30602@gmail.com>
2024-09-09 13:07:41 +0530

    accountService Changes

--- model/User.js
+++ model/User.js
@@ -2,9 +2,7 @@
 
 const admin = require("firebase-admin");
 
-
-const serviceAccount =  JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
-
+const serviceAccount = require("../serviceAccount.json") || JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
